f751b60b731bbcd734b2a658211e78e794e20bec
jbautista <joseluisbautista@gmail.com>
2019-02-03 08:30:02 -0600

    Se agrega el tercer proyecto de ejemplo de codigo espagueti.

---
+++ EjemploCodigoEspagueti3/GestorMensajes.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace DesdeLasHorasExtras.EjemploCodigoEspagueti3
+{
+    /// <summary>
+    /// Gestor de mensajes, su misión es guardar mensajes de bitacora en diversos medios.
+    /// </summary>
+    public class GestorMensajes
+    {
+        /// <summary>
+        /// Guarda el mensaje segun el metodo que se indica como parametro
+        /// </summary>
+        /// <param name="modo">Modo para guardar</param>
+        /// <param name="mensaje">Mensaje a guardar</param>
+        public void Guardar(ModoGuardado modo, string mensaje)
+        {
+            switch (modo)
+            {
+                case ModoGuardado.ArchivoPlano:
+                    //Guardo la información en el archivo de texto
+                    GuardarArchivoPlano(mensaje);
+                    break;
+
+                case ModoGuardado.BaseDatos:
+                    //Guardo la información en la base de datos
+                    GuardarBaseDatos(mensaje);
+                    break;
+
+                case ModoGuardado.VisorEventos:
+                    //Lo guardo en el Visor de eventos de Windows
+                    GuardarVisorEventos(mensaje);
+                    break;
+
+                default:
+                    throw new InvalidOperationException($"No se reconoce el tipo de guardado {modo}");
+            }
+        }
+
+        /// <summary>
+        /// Guarda el mensaje dentro de un archivo plano
+        /// </summary>
+        /// <param name="mensaje">Mensaje a guardar</param>
+        private static void GuardarArchivoPlano(string mensaje)
+        {
+            string directorioArchivo = AppDomain.CurrentDomain.BaseDirectory;
+            string rutaArchivo = Path.Combine(directorioArchivo, "bitacora.txt");
+
+            File.AppendAllText(rutaArchivo, $"{mensaje}{Environment.NewLine}");
+        }
+
+        /// <summary>
+        /// Configura la base de datos, creandola si no existe
+        /// </summary>
+        private static void ConfigurarBaseDatos()
+        {
+            using (EjemploCodigoEspaguetiEntities context = new EjemploCodigoEspaguetiEntities())
+            {
+                //Si no existe la base de datos (archivo mdf) lo creo
+                string directorioBaseDatos = AppDomain.CurrentDomain.BaseDirectory;
+                AppDomain.CurrentDomain.SetData("DataDirectory", directorioBaseDatos);
+                string rutaBaseDatos = Path.Combine(directorioBaseDatos, @"EjemploCodigoEspagueti.mdf");
+
+                context.Database.CreateIfNotExists();
+            }
+        }
+
+        /// <summary>
+        /// Guarda el mensaje dentro de una base de datos
+        /// </summary>
+        /// <param name="mensaje">Mensaje a guardar</param>
+        private static void GuardarBaseDatos(string mensaje)
+        {
+            using (EjemploCodigoEspaguetiEntities context = new EjemploCodigoEspaguetiEntities())
+            {
+                ConfigurarBaseDatos();
+
+                //Agrego el mensaje en la base de datos
+                context.Bitacora.Add(new Bitacora { mensaje = mensaje });
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Crea la fuente para el visor de eventos.
+        /// </summary>
+        private static void ConfigurarVisorEventos()
+        {
+            //Si no existe en memoria lo creo
+            string source = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            string log = "Application";
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, log);
+            }
+        }
+
+        /// <summary>
+        /// Guarda el mensaje en el visor de eventos de windows
+        /// </summary>
+        /// <param name="mensaje">Mensaje a guardar</param>
+        private static void GuardarVisorEventos(string mensaje)
+        {
+            ConfigurarVisorEventos();
+            string source = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+
+            //Guardo el mensaje.
+            System.Diagnostics.EventLog appLog = new System.Diagnostics.EventLog();
+            appLog.Source = source;
+            appLog.WriteEntry(mensaje);
+        }
+    }
+}
---
+++ EjemploCodigoEspagueti3/ModoGuardado.cs
@@ -0,0 +1,23 @@
+﻿namespace DesdeLasHorasExtras.EjemploCodigoEspagueti3
+{
+    /// <summary>
+    /// Define el destino del mensaje
+    /// </summary>
+    public enum ModoGuardado
+    {
+        /// <summary>
+        /// Se guarda en un archivo plano en la ruta del ejecutable
+        /// </summary>
+        ArchivoPlano,
+
+        /// <summary>
+        /// Se guarda en una base de datos
+        /// </summary>
+        BaseDatos,
+
+        /// <summary>
+        /// Se guarda en el visor de eventos de windows.
+        /// </summary>
+        VisorEventos
+    }
+}
