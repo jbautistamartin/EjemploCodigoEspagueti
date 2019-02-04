3053f57ea72e1cbd6f2a0e72dd8f62ec7071a527
jbautista <joseluisbautista@gmail.com>
2019-02-03 22:43:38 -0600

    Se agregran comentarios en la clase de Entity Framework

--- EjemploCodigoEspagueti2/EjemploCodigoEspaguetiModel.Context.cs
+++ EjemploCodigoEspagueti2/EjemploCodigoEspaguetiModel.Context.cs
@@ -13,18 +13,48 @@ namespace DesdeLasHorasExtras.EjemploCodigoEspagueti2
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
+    /// <summary>
+    /// Base de datos de ejemplo de entity framework
+    /// </summary>
+    /// <seealso cref="System.Data.Entity.DbContext" />
     public partial class EjemploCodigoEspaguetiEntities : DbContext
     {
+    	/// <summary>
+    	/// Constructor por defecto.
+    	/// </summary>
         public EjemploCodigoEspaguetiEntities()
             : base("name=EjemploCodigoEspaguetiEntities")
         {
         }
     
+    	/// <summary>
+    	/// This method is called when the model for a derived context has been initialized, but
+    	/// before the model has been locked down and used to initialize the context.  The default
+    	/// implementation of this method does nothing, but it can be overridden in a derived class
+    	/// such that the model can be further configured before it is locked down.
+    	/// </summary>
+    	/// <param name="modelBuilder">The builder that defines the model for the context being created.</param>
+    	/// <exception cref="UnintentionalCodeFirstException"></exception>
+    	/// <remarks>
+    	/// Typically, this method is called only once when the first instance of a derived context
+    	/// is created.  The model for that context is then cached and is for all further instances of
+    	/// the context in the app domain.  This caching can be disabled by setting the ModelCaching
+    	/// property on the given ModelBuidler, but note that this can seriously degrade performance.
+    	/// More control over caching is provided through use of the DbModelBuilder and DbContextFactory
+    	/// classes directly.
+    	/// </remarks>
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
         }
     
+    
+        /// <summary>
+        /// Entidad entitySet.
+        /// </summary>
+        /// <value>
+        /// entitySet.
+        /// </value>
         public virtual DbSet<Bitacora> Bitacora { get; set; }
     }
 }
